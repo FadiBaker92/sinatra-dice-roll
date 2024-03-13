062e8bc255e42827da7be8ac55a310171b176233
FADI BAKER <133148112+FadiBaker92@users.noreply.github.com>
2024-03-13 19:23:13 +0000

    fixed all the outcome var and turned them into instance var

--- dice.rb
+++ dice.rb
@@ -20,7 +20,7 @@ get("/dice/2/6") do
   first_dice = rand(1..6)
   second_dice = rand(1..6)
   sum = first_dice + second_dice
-  outcome = "You rolled #{first_dice} and #{second_dice} and the sum is #{sum}"
+  @outcome = "You rolled #{first_dice} and #{second_dice} and the sum is #{sum}"
   erb(:two_six)
 end
 
@@ -28,13 +28,13 @@ get("/dice/2/10") do
   first_dice = rand(1..10)
   second_dice = rand(1..10)
   sum = first_dice + second_dice
-  outcome = "You rolled #{first_dice} and #{second_dice} and the sum is #{sum}"
+  @outcome = "You rolled #{first_dice} and #{second_dice} and the sum is #{sum}"
   erb(:two_ten)
 end
 
   get("/dice/1/20") do
     first_dice = rand(1..20)
-    outcome = "You rolled #{first_dice}"
+    @outcome = "You rolled #{first_dice}"
    erb(:one_twenty)
   end
 
@@ -45,7 +45,7 @@ end
     dice_4 = rand(1..4)
     dice_5 = rand(1..4)
     sum = dice_1 + dice_2 + dice_3 + dice_4 + dice_5
-    outcome = "You rolled #{dice_1}, #{dice_2}, #{dice_3}, #{dice_4} and #{dice_5} and the sum is #{sum}"
+    @outcome = "You rolled #{dice_1}, #{dice_2}, #{dice_3}, #{dice_4} and #{dice_5} and the sum is #{sum}"
     erb(:five_four)
 
   end
