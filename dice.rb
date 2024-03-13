7d1ee2fcf441e42f880b91984fde71fff2def113
FADI BAKER <133148112+FadiBaker92@users.noreply.github.com>
2024-03-13 18:55:00 +0000

    added 2 new gems for debugging and required them in the dice.rb

--- dice.rb
+++ dice.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require "better_errors"
+require "binding_of_caller"
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/hello") do 
   
@@ -8,7 +13,8 @@ end
 
 get("/") do 
 
-   "<h1> Dice Roll </h1>
+   "
+   <h1> Dice Roll </h1>
    <ul>
       <li><a href='/dice/2/6'>Roll two 6-sided dice</a></li>
       <li><a href='/dice/2/10'>Roll two 10-sided dice</a></li>
